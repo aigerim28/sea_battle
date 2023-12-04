de4ac86917492ac5d62f43443ff0eb998d6202d7
aigerim28 <150646854+aigerim28@users.noreply.github.com>
2023-12-05 00:06:58 +0600

    Update sea_battle.py
    Added comments

--- sea_battle.py
+++ sea_battle.py
@@ -1,57 +1,26 @@
 import random
 
+#The list for all players, with their scores.
 all_players = []
 
+#The function for cleaning screen
 def cls():
     print('\n' * 100)
 
+#Key for sorting scores in list of all players
 def custom_key(players_list):
     return players_list[1]
 
-def main_menu():
-    print('You are in Main menu. ')
-    print('\nActions: \n1 - Start a game \n2 - See the list of all players')
-    print()
-    choose_action()
 
-def choose_action():
-    action = int(input('Choose action (1/2): '))
-    if action == 1:
-        cls()
-        sea_battle()
-    elif action == 2:
-        if all_players:
-            cls()
-            print('List of players is empty. Become first!')
-            print()
-            menu = int(input('For going back to Main menu enter 1: '))
-            if menu == 1:
-                cls()
-                main_menu()
-        else:
-            all_players.sort(key=custom_key, reverse=True)
-            cls()
-            print('List of all players:')
-            for i in range(len(all_players)):
-                print(f'{i + 1}. {all_players[i][0]} - {all_players[i][1]}')
-            print()
-            
-            menu = int(input('For going back to Main menu enter 1: '))
-            if menu == 1:
-                cls()
-                main_menu()
-    else:
-        print('There is no such action. PLease, try again.')
-        choose_action()
-
+#This function creates a console which will be shown to user, with first row consisting of letters A-G and first column with numbers 1-7
 def create_console():
-    y_domain = [0, 'A', 'B', 'C', 'D', 'E', 'F', 'G']
     console = [[0 for _ in range(8)] for _ in range(8)]
-    console[0] = y_domain
+    console[0] = [0, 'A', 'B', 'C', 'D', 'E', 'F', 'G']
     for i in range(1, 8):
         console[i][0] = i
     return console
 
+#Function for creating 8 different maps where ships are located and it gives to user randomly one of them.
 def place_ships():
     list_ships = []
     ships1 = [[0, 0, 0, 0, 0, 0, 0],
@@ -152,6 +121,10 @@ def place_ships():
     list_ships.append(ships8)
     return random.choice(list_ships)
 
+#hit_point function gets console, ships placement and x, y coordinates
+#and checks whether the given point has a ship in there or not and gives True 
+#or False. Furthermore,it checks did the user totally crushed one ship or just hit it, and 
+#prints such message. 
 def hit_point(console, ships, x, y):
     if ships[x + 1][y - 1] == 1:
         print('You have crushed a ship!')
@@ -184,7 +157,12 @@ def hit_point(console, ships, x, y):
         console[x][y] = '-'
         return False
 
-
+#The function part where the game starts. It takes a name from user,
+#gets coordinates until there is no ship on the map, counts hits and 
+#appends name with number of hits to the all players' score list.
+#After ending a game it asks user about their will to play again.
+#So, if answer is yes, new game starts, if answer is no, it moves user
+#back to Main menu.
 def sea_battle():
     result = []
     name = input('Enter your name: ')
@@ -195,9 +173,13 @@ def sea_battle():
     y_domain = [0, 'A', 'B', 'C', 'D', 'E', 'F', 'G']
     console = create_console()
     ships = place_ships()
+    #Variable for counting hits 
     hits = 0
+    #Variable for counting points for each hit ship.
     total_points = 0
-    
+
+    #Totally there are 11 places where ships are present.
+    #So, we will continue the game until we crush all ships
     while total_points < 11:
         for row in console:
             print(' '.join([str(elem) for elem in row]))
@@ -213,7 +195,9 @@ def sea_battle():
             y_letter, x = input().split(';')
             y = y_domain.index(y_letter)
             x = int(x)
-        
+
+        #If we get True value (so we hit or crush ship) and we 
+        #add it to variable total_point
         if hit_point(console, ships, x, y) == True:
             total_points += 1
         hits += 1
@@ -221,16 +205,58 @@ def sea_battle():
         print('Congratulations! You have crushed all ships!')
         print('Quantity of hits you made:', hits)
 
+        #Appending user's name and score to the list result firstly
+        #and appending result list to list of all_players
         result.append(name)
         result.append(hits)
         all_players.append(result)
-        
+
+        #Asking user about their will to play again.
         will = int(input('\nWould you like to play one more time? (Enter 1/0)'))
         if will == 1:
             sea_battle()
         else:
             main_menu()
 
+
+#The function for choosing actions: starting a game or seeing list of all players with their scores.
+def choose_action():
+    action = int(input('Choose action (1/2): '))
+    if action == 1:
+        cls()
+        sea_battle()
+    elif action == 2:
+        if all_players:
+            cls()
+            print('List of players is empty. Become first!')
+            print()
+            menu = int(input('For going back to Main menu enter 1: '))
+            if menu == 1:
+                cls()
+                main_menu()
+        else:
+            #Sorting all_players list from highest to lowest and printing it.
+            all_players.sort(key=custom_key, reverse=True)
+            cls()
+            print('List of all players:')
+            for i in range(len(all_players)):
+                print(f'{i + 1}. {all_players[i][0]} - {all_players[i][1]}')
+            print()
+            
+            menu = int(input('For going back to Main menu enter 1: '))
+            if menu == 1:
+                cls()
+                main_menu()
+    else:
+        #If user enters invalid action that does not exist, program will tell it and ask to enter again.
+        print('There is no such action. PLease, try again.')
+        choose_action()
+#Function for Main menu
+def main_menu():
+    print('You are in Main menu. ')
+    print('\nActions: \n1 - Start a game \n2 - See the list of all players')
+    print()
+    choose_action()
 
 main_menu()
 
